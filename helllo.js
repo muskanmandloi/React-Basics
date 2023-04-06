4f62bb7f56406f93ba8c565d9ce770a8ae340dc5
MuskanMandloi <54906865+muskanmandloi@users.noreply.github.com>
2023-04-07 00:01:04 +0530

    commit
    added new file

---
+++ helllo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const helllo = () => {
+  return (
+    class Car extends React.Component {
+        render() {
+          return <h2>Hi, I am a Car!</h2>;
+        }
+      }
+  )
+}
